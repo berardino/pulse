93792862afbbe7b628f95911beaa4c8fa8fdedc4
Berardino la Torre <bto@tradeshift.com>
2017-09-28 13:40:16 -0700

    Add spark hello world

--- src/main/scala/com/tradeshift/pulse/Main.scala
+++ src/main/scala/com/tradeshift/pulse/Main.scala
@@ -1,8 +1,11 @@
 package com.tradeshift.pulse
 
+import javax.annotation.PreDestroy
+
 import com.tradeshift.pulse.config._
+import org.apache.spark.sql.SparkSession
 import org.springframework.boot.SpringApplication
-import org.springframework.context.annotation.{Configuration, Import}
+import org.springframework.context.annotation.{Bean, Configuration, Import}
 
 
 @Configuration
@@ -10,9 +13,31 @@ import org.springframework.context.annotation.{Configuration, Import}
   classOf[ServerConfiguration],
   classOf[JerseyConfiguration],
   classOf[JacksonConfiguration],
-  classOf[PropertiesConfiguration],
+  classOf[PropertiesConfiguration]
 ))
-class Main
+class Main {
+  @Bean
+  def spark(): SparkApp = {
+    new SparkApp()
+  }
+}
+
+class SparkApp {
+  val logFile = "/tmp/RandomFile.txt"
+  val spark = SparkSession.builder.appName("Simple Application").master("local[2]").getOrCreate()
+
+
+  val logData = spark.read.textFile(logFile).cache()
+  val numAs = logData.filter(line => line.contains("a")).count()
+  val numBs = logData.filter(line => line.contains("b")).count()
+  println(s"Lines with a: $numAs, Lines with b: $numBs")
+
+
+  @PreDestroy
+  def stop(): Unit = {
+    spark.stop()
+  }
+}
 
 object Main {
   def main(args: Array[String]) {
