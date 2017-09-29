2b9cee19ad24ddba36ee39b88012acdd8cb6cdc6
Berardino la Torre <bto@tradeshift.com>
2017-09-29 09:41:05 -0700

    Generate random events

--- src/main/scala/com/tradeshift/pulse/Main.scala
+++ src/main/scala/com/tradeshift/pulse/Main.scala
@@ -2,15 +2,19 @@ package com.tradeshift.pulse
 
 import java.io.InputStreamReader
 import java.sql.Timestamp
-import java.util.Properties
-import javax.annotation.PreDestroy
+import java.time.Instant
+import java.util.{Locale, Properties}
+import javax.annotation.{PostConstruct, PreDestroy}
 
+import com.tradeshift.pulse.GEOService.randomCountryIso
 import com.tradeshift.pulse.config._
-import com.tradeshift.pulse.dto.{AuditEvent, SSE, TradeshiftDispatch}
-import org.apache.spark.sql.SparkSession
-import org.mortbay.util.ajax.JSON
+import org.apache.spark.sql.catalyst.expressions.GenericRowWithSchema
+import org.apache.spark.sql.{SaveMode, SparkSession}
 import org.springframework.boot.SpringApplication
 import org.springframework.context.annotation.{Bean, Configuration, Import}
+import org.springframework.core.env.Environment
+
+import scala.util.Random
 
 
 @Configuration
@@ -19,53 +23,80 @@ import org.springframework.context.annotation.{Bean, Configuration, Import}
   classOf[JerseyConfiguration],
   classOf[JacksonConfiguration],
   classOf[PropertiesConfiguration],
-  classOf[JdbcConfiguration],
-  classOf[MigrationConfiguration]
+  classOf[JdbcConfiguration]
+  //classOf[MigrationConfiguration]
 ))
 class Main {
   @Bean
-  def spark(): SparkApp = {
-    new SparkApp()
+  def spark(env: Environment): SparkApp = {
+    new SparkApp(env)
   }
 }
 
-class SparkApp {
-  val logFile = "/tmp/events.txt"
-  val spark = SparkSession.builder.appName("Simple Application").master("local[2]").getOrCreate()
+case class AuditEvent(source: String, dest: String, timestamp: Timestamp)
 
+case class AggregateEvent(source: String, dest: String, start_at: Timestamp, end_at: Timestamp, volume: Long)
+
+object Time extends Serializable {
+  def randomTime = {
+    if (Random.nextInt(1) == 0) {
+      new Timestamp(Instant.now().plusSeconds(Random.nextInt(5)).toEpochMilli)
+    } else {
+      new Timestamp(Instant.now().minusSeconds(Random.nextInt(5)).toEpochMilli)
+    }
+  }
+
+}
+
+class SparkApp(val env: Environment) {
+  val spark = SparkSession.builder.appName("Pulse").master("local[2]").getOrCreate()
+
+  import org.apache.spark.sql.functions._
   import spark.implicits._
 
-  val logData = spark.read.json(logFile).as[AuditEvent]
-  logData.show()
-  val result = logData.filter(line => line.channel.map(c => "TRADESHIFT".equals(c.id)).getOrElse(false))
-    .map(ProcessEvent.apply)
+  val numRecords: Int = 50000
+  val partitions: Int = 1
+  val recordsPerPartition = numRecords / partitions
 
-  val properties = new Properties();
-  properties.setProperty("driver", "org.postgresql.Driver")
-  properties.setProperty("user", "pulse")
-  //result.write.mode("append").jdbc("jdbc:postgresql://192.168.99.100:6432/pulse", "events_per_second", properties);
+  val properties = new Properties()
+  properties.setProperty("driver", env.getProperty("database.driver"))
+  properties.setProperty("user", env.getProperty("database.user"))
 
-  result.show()
+  val seedRdd = spark.sparkContext.parallelize(Seq.fill(partitions)(recordsPerPartition), partitions)
+  val randomEvents = seedRdd.flatMap(records => Seq.fill(records)(
+    AuditEvent(randomCountryIso, randomCountryIso, Time.randomTime))).toDS()
 
+
+  val toWindowStart = udf { (window: GenericRowWithSchema) =>
+    window.getTimestamp(0)
+  }
 
   @PreDestroy
   def stop(): Unit = {
     spark.stop()
   }
-}
 
-
-object ProcessEvent extends Serializable {
-
-  def apply(event: AuditEvent): SSE = {
-    val data = TradeshiftDispatch("US", "FR", "7765543")
-    SSE(new Timestamp(event.timestamp), "document-sent", JSON.toString(data))
+  @PostConstruct
+  def startProcessing() = {
+    randomEvents
+      //.filter(line => line.channel.map(c => "TRADESHIFT".equals(c.id)).getOrElse(false))
+      .groupBy(col("source"), col("dest"), window($"timestamp", "5 seconds") as ("start_at"))
+      .count()
+      .map(r => AggregateEvent(
+        r.getString(0),
+        r.getString(1),
+        r.get(2).asInstanceOf[GenericRowWithSchema].getTimestamp(0),
+        r.get(2).asInstanceOf[GenericRowWithSchema].getTimestamp(1),
+        r.getLong(3)))
+      .write
+      .mode(SaveMode.Append)
+      .jdbc(env.getProperty("database.url"), "aggregated_events", properties);
   }
 }
 
 case class Country(iso: String, lat: String, lon: String)
 
-object GEOLookUp {
+object GEOService {
 
   import com.github.tototoshi.csv._
 
@@ -80,9 +111,21 @@ object GEOLookUp {
   }).toMap
   reader.close()
 
+  val locales = Locale.getISOCountries
 
-  def lookup(iso: String): Country = {
-    map(iso)
+
+  def lookup(iso: String): Option[Country] = map.get(iso)
+
+  def toCountry(iso: String): Country = {
+    lookup(iso).getOrElse(lookup("US").get)
+  }
+
+  def randomCountryIso(): String = {
+    locales(Random.nextInt(20))
+  }
+
+  def randomCountry(): Country = {
+    toCountry(randomCountryIso())
   }
 }
 
--- src/main/scala/com/tradeshift/pulse/rest/EventsResource.scala
+++ src/main/scala/com/tradeshift/pulse/rest/EventsResource.scala
@@ -1,12 +1,12 @@
 package com.tradeshift.pulse.rest
 
-import java.util.Locale
 import javax.inject.{Inject, Singleton}
 import javax.ws.rs.core.MediaType
 import javax.ws.rs.{GET, Produces}
 
 import com.fasterxml.jackson.databind.ObjectMapper
-import com.tradeshift.pulse.{Country, GEOLookUp}
+import com.tradeshift.pulse.GEOService.randomCountry
+import com.tradeshift.pulse.{Country, GEOService}
 import org.glassfish.jersey.media.sse.{EventOutput, OutboundEvent, SseFeature}
 
 import scala.util.Random
@@ -17,38 +17,35 @@ case class Event(source: Country, dest: Country, volume: Int)
 @Singleton
 class EventsResource @Inject()(mapper: ObjectMapper) {
 
-
-  val locales = Locale.getISOCountries
-
   @volatile var eventOutput = new EventOutput
 
-  def randomCountry(): Country = {
-    GEOLookUp.lookup(locales(Random.nextInt(locales.length)))
-  }
+  import org.glassfish.jersey.media.sse.SseBroadcaster
+
+  private val broadcaster = new SseBroadcaster
+
+  new Thread() {
+    override def run(): Unit = {
+      try {
+        while (true) {
+          val event = Event(randomCountry(), randomCountry(), Math.abs(Random.nextInt(1000000)))
+          val payload = mapper.writeValueAsString(event)
+          val sse = new OutboundEvent.Builder().mediaType(MediaType.APPLICATION_JSON_TYPE).data(classOf[String], payload).build
+          broadcaster.broadcast(sse)
+          Thread.sleep(2000)
+        }
+      } catch {
+        case e@(_: InterruptedException) =>
+          e.printStackTrace
+      }
+    }
+  }.start()
 
   @GET
   @Produces(Array(SseFeature.SERVER_SENT_EVENTS))
   def getEvents(): EventOutput = {
-    val seq = new EventOutput
-
-    new Thread() {
-      override def run(): Unit = {
-        try {
-          (1 to 100).foreach { id =>
-            val event = Event(randomCountry, randomCountry, Math.abs(Random.nextInt(1000000)))
-            val payload = mapper.writeValueAsString(event)
-            val sse = new OutboundEvent.Builder().mediaType(MediaType.APPLICATION_JSON_TYPE).data(classOf[String], payload).build
-            seq.write(sse)
-            Thread.sleep(2000)
-          }
-          seq.close()
-        } catch {
-          case e@(_: InterruptedException) =>
-            e.printStackTrace
-        }
-      }
-    }.start()
-    seq
+    val out = new EventOutput
+    broadcaster.add(out)
+    out
   }
 
 }
